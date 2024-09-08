4de6b0858f853417a68d0416b139a4c11f57e779
Rocio Belen Acosta <rooacost@gmail.com>
2024-09-07 23:27:32 -0300

    Agregar archivos de proyecto.

---
+++ EjVariables/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjVariables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sumando1 = 10;
+            int suma2 = 50;
+            int suma3 = 40;
+            int resultado;
+
+            resultado = sumando1 + suma2 + suma3;
+
+            label1.Text = resultado.ToString();
+        }
+    }
+}
